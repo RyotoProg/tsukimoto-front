b9615fe0d1df7cd9fe920d493940bd9787b23815
RyotoProg <benjamin.valenzuelaem@gmail.com>
2024-11-23 21:10:21 -0300

    config

--- app/(admin)/tsk/admin/(dash)/config/page.tsx
+++ app/(admin)/tsk/admin/(dash)/config/page.tsx
@@ -5,6 +5,10 @@ import React from 'react'
 export default async function Config() {
 
     var config = await getConfig()
+    
+    if(config.genres.length === 0){
+      config.genres = [{value: false, label: 'porfavor agregue generos'}]
+    }
 
   return (
     <div className='px-4 py-4 md:px-10 mx-auto w-full text-[#dddddd]'>
