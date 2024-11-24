fa85bb8471e1f4c7b495686ba273d9b591a0d039
RyotoProg <benjamin.valenzuelaem@gmail.com>
2024-11-24 15:10:23 -0300

    .next

---
+++ .next/static/chunks/app/(admin)/tsk/admin/(dash)/news/preview/[id]/page-21d058d66b632efe.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[841],{9482:(e,s,_)=>{Promise.resolve().then(_.bind(_,508))}},e=>{var s=s=>e(e.s=s);e.O(0,[839,551,476,508,441,517,358],()=>s(9482)),_N_E=e.O()}]);
